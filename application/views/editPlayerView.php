40fa5fce54432b503cd6a2ea51088b2d44ecfccf
jtekenos <jessicatekenos@gmail.com>
2015-11-11 19:13:40 -0800

    player deletion complete

--- application/views/editPlayerView.php
+++ application/views/editPlayerView.php
@@ -16,11 +16,16 @@
 		<td>{lastname}</td>
 		<td>{number}</td>
 		<td>{position}</td>
-		<td><a href = '/admin/edit'>Edit</a></td>
+		<td><a href = '/admin/edit/{id}'>Edit</a><br>
+			<a href = '/admin/delete/{id}'>Delete</a>
+		</td>
 	</tr>
 	<tr></tr>
 
 	{/players}
 
 </table>
+
+
+
 <a href = './admin/add'>Switch to Add Player</a>
