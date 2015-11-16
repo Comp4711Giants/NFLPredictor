99b58fee28b5eca8cef667ce45fcb657e270a2d2
jtekenos <jessicatekenos@gmail.com>
2015-11-15 23:27:59 -0800

    invalid css

--- application/views/playersView.php
+++ application/views/playersView.php
@@ -4,7 +4,7 @@
 
 <div class="row">
     {players}
-    <div class="span4" align="center">
+    <div class="span4">
         <img src="/data/mugs/{mug}" title="{who}"/></br>
         <a href="/Player/display/{id}"><p class = "lead">{who}</p></a>
         <p>#{number}, {position}</p>
